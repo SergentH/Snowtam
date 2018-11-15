a127230fc3552f3fbaf365b4cc571a9a61b28979
SergentH <hugo.sergent37@gmail.com>
2018-11-15 09:55:32 +0100

    Création de la deuxième activitée

---
+++ app/src/main/java/com/example/hugo/snowtam_app/controller/main/ResultActivity.java
@@ -0,0 +1,15 @@
+package com.example.hugo.snowtam_app.controller.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.hugo.snowtam_app.R;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
