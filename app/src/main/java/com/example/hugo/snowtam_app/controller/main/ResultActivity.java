803e27fb83ffa4a5ba76b85abe23cbbd3704c38d
SergentH <hugo.sergent37@gmail.com>
2018-11-24 17:10:46 +0100

    google map ok

--- app/src/main/java/com/example/hugo/snowtam_app/controller/main/ResultActivity.java
+++ app/src/main/java/com/example/hugo/snowtam_app/controller/main/ResultActivity.java
@@ -9,13 +9,26 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.hugo.snowtam_app.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class ResultActivity extends AppCompatActivity{
+public class ResultActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) this.getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         /*lien avec des differents elements graphiques*/
         final TextView textTitle =  findViewById(R.id.textTitle);
@@ -120,5 +133,15 @@ public class ResultActivity extends AppCompatActivity{
             }
         });
 
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
